a9e91a8feb56214a0be7c780167692ad6154f98a
CryptoBender25 <111506801+CryptoBender25@users.noreply.github.com>
2022-08-18 13:58:26 +0200

    Update gen_ava.py

--- gen_ava.py
+++ gen_ava.py
@@ -1,9 +1,10 @@
+from urllib import request
 from requests import get
 import time
 from os import mkdir
 
 try:
-    mkdir("avas")
+    mkdir("avas")https://github.com/CryptoBender25/generate_avatar/blob/main/gen_ava.py
 except:
     pass
 def get_random_avatar_from_web():
@@ -11,8 +12,9 @@ def get_random_avatar_from_web():
         # print(r.content)
         # print(b64encode(r.content))
         return(r.content)
-        
-for i in range(50,80):
+
+i = input("Сколько ав генерировать")
+for i in range(1, i+1):
     with open(f"avas/image{i}.png", "wb") as f:
         f.write(get_random_avatar_from_web())
     time.sleep(1)
